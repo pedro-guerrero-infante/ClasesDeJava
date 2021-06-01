b3023490e8b3ab13c99117f86990faf4e515383a
proxar <pedroguerrero902@gmail.com>
2021-05-31 20:55:36 -0500

    primer commit de pedro

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -4,7 +4,14 @@ public class Controlador {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("trabajos get post");
+		System.out.println("trabajos get post");
+		
+		int n = 0;
+		while(n < 10)
+		{
+			System.out.println(n);
+			n++;
+		}
 	}
 
 }
